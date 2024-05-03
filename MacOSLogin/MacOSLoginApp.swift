fc467544e15ee4e9cf0bf3e04eea78397be7cabc
Volodymyr <zmiyov88@gmail.com>
2024-05-03 13:47:55 +0300

    Initial Commit

---
+++ MacOSLogin/MacOSLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSLoginApp.swift
+//  MacOSLogin
+//
+//  Created by Vladimir Pisarenko on 03.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MacOSLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
